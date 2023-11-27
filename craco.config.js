c2a7c7cd3517e855e73eda49c67ea83cb23e25d8
Thibaut Decherit <thibaut.decherit@protonmail.com>
2023-11-27 16:02:53 +0100

    continuation of 2e2919361ed0796d94eb2f69a475bf43057501ce

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,13 @@
 const path = require('path');
 module.exports = {
+    jest: {
+        configure: {
+            moduleNameMapper: {
+                "^@/(.*)$": "<rootDir>/src/$1"
+            },
+            preset: 'ts-jest'
+        }
+    },
     webpack: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
