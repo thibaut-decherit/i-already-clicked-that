15ccd5a7f2458e5cb01ab48ca46eb57efeed3de4
Thibaut Decherit <thibaut.decherit@protonmail.com>
2023-12-11 13:57:59 +0100

    feat(filter): list

--- src/features/filter/routes/Filters/Filters.tsx
+++ src/features/filter/routes/Filters/Filters.tsx
@@ -1,13 +1,34 @@
 import {Layout} from "@/components/Layout";
 import {NavbarLinkIcon} from "@/components/Navbar";
+import {useFiltersStore} from "@/features/filter/stores/filters";
+import {List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 
 const Filters = () => {
+  const {filters} = useFiltersStore();
+
+  const generateListItems = () => {
+    return filters.map((filter) => {
+      return (
+        <ListItem disablePadding divider={true} key={filter.id}>
+          <ListItemButton>
+            <ListItemIcon>
+              <span className="fa fa-globe"/>
+            </ListItemIcon>
+            <ListItemText>{filter.name}</ListItemText>
+          </ListItemButton>
+        </ListItem>
+      );
+    });
+  };
+
   return (
     <Layout
       navbarRight={<NavbarLinkIcon ariaLabel="Add filter" iconClass="fa-plus" to="/filter/new"/>}
       title="Filters"
     >
-      <span>TODO</span>
+      <List>
+        {generateListItems()}
+      </List>
     </Layout>
   );
 }
